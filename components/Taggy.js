e691b03c81b1fb4516a8ea11a10dfbe72ebe6314
Fsalker <Andreiii500@yahoo.com>
2016-11-19 17:45:40 +0200

    Taggy? weird

--- components/Taggy.js
+++ components/Taggy.js
@@ -1,37 +1,31 @@
 import React from 'react';
-//import FormGroup from 'react-bootstrap-form';
-
-//<ControlLabel>Select</ControlLabel>
-
-var tagArray=["Meteo","Taxe","Profesori"];
+import Toateorone from './Toateorone.js';
+var tagArray;
+var key = 0;
 
 const Taggy = (props) => (
   <div>
+  {initTagArray()}
     <h2>text = {props.tagType}</h2>
 	<select>
-	  <option value="volvo">Volvo</option>
-	  <option value="saab">Saab</option>
-	  <option value="mercedes">Mercedes</option>
-	  <option value="audi">Audi</option>
+	{tags_to_html()}
 	</select>
+	<Toateorone/>
   </div>
 )
 
 function tags_to_html()
 {
-	var cod = "";
-	for(var i=0; i<tagArray.length(); ++i))
+	var cod = new Array;
+	cod.push(<option key={key++} value="---">---</option>)
+	for(var i=0; i<tagArray.length; ++i) cod.push(<option key={key++} value={tagArray[i]}>{tagArray[i]}</option>);
+	
+	return cod;
 }
-
-/*class Taggy	 extends React.Component
-{
-  constructor()
-  {
-    super();
-    this.state={
-      TAGtype: "noType"
-  }
-}*/
  
+function initTagArray()
+{
+	tagArray=["Meteo","Taxe","Profesori"];
+}
 
 export default Taggy
